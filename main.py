c17a44179868487640ffeb09f08e480ad98c9bb1
kelvinkuria <kkuria383@yahoo.com>
2024-03-08 13:11:57 +0300

    Grades app done

--- main.py
+++ main.py
@@ -2,11 +2,34 @@
 #If...else
 #If....else..if
 
+#Boolean data types(true,false)
+# #weather app
+# weather = input("Kuko aje (What's the weather like?): ")
 
-#weather app
-weather = 'sunny'
+# if weather == 'rain':
+#     print('Bring an umbrella!')
+# elif weather == 'cloudy':
+#     print('No need for a coat.')
+# else:
+#     print('Dress according to the weather.')
 
-if weather == 'rain':
-    print('umrella')
+# one equal sign(=) used to assign values
+# two equal signs(==) is a comparison operator(==,<.>,<=,=>)
+
+# Grades app
+
+score = 50
+
+if score >= 90:
+    print('Wow, you passed with flying colors!')
+elif 80 <= score < 90:
+    print('You did great!')
+elif 70 <= score < 80:
+    print('Try harder next time.')
+elif 60 <= score < 70:
+    print('Pull up your socks!')
 else:
-  print('no rain')  
+    print('You need to study more.')
+
+
+
