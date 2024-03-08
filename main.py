692f07cd23af321e0b4782c7dde8e9c959292c5f
kelvinkuria <kkuria383@yahoo.com>
2024-03-08 14:20:54 +0300

    functions done

--- main.py
+++ main.py
@@ -18,18 +18,36 @@
 
 # Grades app
 
-score = 50
+#  score = 78
 
-if score >= 90:
-    print('Wow, you passed with flying colors!')
-elif 80 <= score < 90:
-    print('You did great!')
-elif 70 <= score < 80:
-    print('Try harder next time.')
-elif 60 <= score < 70:
-    print('Pull up your socks!')
-else:
-    print('You need to study more.')
+# if score >= 90:
+#     print('Wow, you passed with flying colors!')
+# elif 80 <= score < 90:
+#     print('You did great!')
+# elif 70 <= score < 80:
+#     print('Try harder next time.')
+# elif 60 <= score < 70:
+#     print('Pull up your socks!')
+# else:
+#     print('You need to study more.')
 
 
+# if score >= 60 and score <=100:
+#            print('pass')
 
+
+# Functions - a block of code that performs a specific task. It enables reusability of code modularity and better organisation.
+#Arguments are values passed to a function when it is called.
+# parameters are temporary placeholders for arguments
+# def say_my_name():
+#        print('Kuria')
+
+# say_my_name()
+       
+def say_my_name_2(name):
+    print(name)
+# say_my_name_2('Kuria')
+    
+def greeting(greet,name):
+    print(f'{greet} {name}!')
+greeting('aloha', 'kuria')
